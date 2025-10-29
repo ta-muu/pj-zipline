a30f1a70573568b2528f9428371ab2e1d28f0774
ta-muu <122217578+ta-muu@users.noreply.github.com>
2025-10-29 22:32:29 +0900

    importの修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
