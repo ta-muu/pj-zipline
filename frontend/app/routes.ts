8b39d2b47de222c21d71b2fe10b111428befeac0
ta-muu <122217578+ta-muu@users.noreply.github.com>
2025-10-29 23:44:13 +0900

    ルーティングの変更

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -1,5 +1,6 @@
 import { index, layout, type RouteConfig } from "@react-router/dev/routes";
 
 export default [
-	layout("./routes/layout.tsx", [index("./routes/taskview.tsx")]),
+	index("./routes/home.tsx"),
+	index("./routes/taskview.tsx"),
 ] satisfies RouteConfig;
